c3fac9b9d515900fd0fc48a4c26cd6219b1408bb
AAROHI TIWARI <67629456+aarohi-tiwari@users.noreply.github.com>
2023-10-07 04:51:43 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -71,3 +71,22 @@ searchInput.addEventListener('blur', () => {
         suggestionList.innerHTML = '';
     }, 200);
 });
+const stars=document.querySelectorAll('.star');
+const current_rating=document.querySelector('.current-rating');
+
+stars.forEach((star,index)=>{
+  star.addEventListener('click',()=>{
+
+    let current_star=index+1;
+    current_rating.innerText=`${current_star} of 5`;
+
+    stars.forEach((star,i)=>{
+        if(current_star>=i+1){
+          star.innerHTML='&#9733;';
+        }else{
+          star.innerHTML='&#9734;';
+        }
+    });
+
+  });
+});
