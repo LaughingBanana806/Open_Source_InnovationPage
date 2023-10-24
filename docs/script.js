a0a73d4668e9a785af8dfaba7122079c23ff1971
Garima-Joshi <65150730+Garima-Joshi@users.noreply.github.com>
2023-10-24 17:39:29 +0530

    Next Page and View More features

--- docs/script.js
+++ docs/script.js
@@ -3,9 +3,25 @@ document.addEventListener('DOMContentLoaded', function() {
     const addProjectContent = document.getElementById('add-project-content');
     const projectForm = document.getElementById('project-form');
     const submitButton = projectForm.querySelector('button');
-
-    addProjectIcon.addEventListener('click', function() {
-        addProjectIcon.style.display = 'none'; // Hide the icon
-        addProjectContent.style.display = 'block'; // Show the form
-    });
+    // addProjectIcon.addEventListener('click', function() {
+    //     addProjectIcon.style.display = 'none'; // Hide the icon
+    //     addProjectContent.style.display = 'block'; // Show the form
+    // });
 });
+
+function listViewToggle(listType,button){
+    const list=document.getElementById(listType)
+    if(list.classList.contains("listExpanded")){
+        list.classList.remove("listExpanded")
+        list.classList.add("listShortened")
+
+        button.innerText="View More";
+
+    }
+    else {
+        list.classList.add("listExpanded")
+        list.classList.remove("listShortened")
+        button.innerText="View Less";
+
+    }
+}
